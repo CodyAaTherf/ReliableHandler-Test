b74aee81bbd0c12905166ee9c69180096f68f8c2
CodyAaTherf <aatherfgamerplayzs@gmail.com>
2022-04-10 23:11:36 +0530

    Found code for "setup"

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const Discord = require('discord.js')
+const { Intents } = require('discord.js')
+const ReliableHandler = require('reliablehandler')
+
+const config = require('./config.json')
+
+const client = new Discord.Client({
+    intents: [
+        Intents.FLAGS.GUILDS
+    ]
+})
+
+client.on('ready' , () => {
+    console.log(`Logged in as ${client.user.tag}!`)
+
+    new ReliableHandler(client)
+})
+
+client.login(config.TOKEN)
